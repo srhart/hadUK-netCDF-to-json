72ed13605b3fb5228a2c3bc8f99159e1653c8d93
srhart <2002987+srhart@users.noreply.github.com>
2022-10-12 09:26:07 +0100

    Initial commit

---
+++ dataview.py
@@ -0,0 +1,85 @@
+import netCDF4
+import numpy as np
+import json
+from datetime import datetime, timedelta
+
+epoc = datetime(1800,1,1,0,0,0)
+startDate = 0
+
+#filename = 'sun_hadukgrid_uk_region_mon-20y_198101-200012'
+filename = 'sun_hadukgrid_uk_region_mon_191901-202112'
+
+f = netCDF4.Dataset(filename + '.nc')
+
+#print(f)
+print("Reading .nc file...")
+md = {}
+for x,y in vars(f).items():
+    md[x] = y
+
+dims ={}
+for d in f.dimensions.keys():
+    dims[d] = {'name' : str(f.dimensions[d].name), 'size' : f.dimensions[d].size }
+md['dimensions'] = dims
+
+vvars = {}
+for k in f.variables.keys():
+    locals()[k] = f.variables[k]
+    vvars[k] = {'dtype' : str(f.variables[k].dtype), 'dimensions' : str(f.variables[k].dimensions), 'attributes' : f.variables[k].__dict__}
+md['variables'] = vvars
+
+rn=[]
+for x in region:
+    rn.append(int(x))
+md['region'] = rn
+
+rn=[]
+for x in geo_region:
+    rn.append(''.join(x[:].astype(str)).rstrip())
+md['geo_region'] = rn
+
+#print("{}".format(time[:]))
+rn=[]
+for x in time[startDate:]:
+    rn.append(int(x))
+md['time'] = rn
+
+
+rn = []
+for i, x in enumerate(time_bnds[startDate:]):
+    ro = {}
+    ro['band'] = i
+    ro['start'] = int(x[0:1])
+    ro['end'] = int(x[1:])
+    ro['month'] = (epoc + timedelta(hours=int(x[0:1]))).strftime('%m')
+    ro['year'] = (epoc + timedelta(hours=int(x[0:1]))).strftime('%Y')
+    rn.append(ro)
+md['time_bnds'] = rn
+#print(rn)
+
+rn=[]
+for i, x in enumerate(sun[startDate:]):
+    ro = {}
+    ro['time_bnd'] = i
+    sn = []
+    for ii, y in enumerate(x, start = 1):
+        so = {}
+        #print("{}".format(y))
+        so['region'] = ii
+        if y is np.ma.masked:
+            so['hours'] = 0
+        else:
+            so['hours'] = y
+        sn.append(so)
+    ro['regions'] = sn
+    rn.append(ro)
+    # print(x)
+md['sun'] = rn
+#print(rn)
+
+#print(json.dumps(md, indent=4))
+f.close()
+
+print("Writing .json file....")
+with open(filename + '.json', 'w', encoding='utf-8') as f:
+    json.dump(md, f, ensure_ascii=False, check_circular=True)
